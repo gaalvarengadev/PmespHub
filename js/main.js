0973aba0e836143257f48dea463ae3cffa9624ae
Gabriel de Alvarenga <gaalvarenga.dev@gmail.com>
2026-02-20 13:50:46 -0300

    escolha do tipo de concurso em andamento

--- js/main.js
+++ js/main.js
@@ -1,30 +1,33 @@
-/*evento do botao da pagina TAF*/
-const verMais = document.getElementById("verMais");
-const sexoDialog = document.getElementById("sexoDialog");
+//botao do Ver mais
 
-const btnMasculino = document.getElementById("btnMasculino");
-const btnFeminino = document.getElementById("btnFeminino");
-const btnCancelar = document.getElementById("btnCancelar");
+const verMais = document.getElementById("verMais")
 
-// 1) Quando clicar no "Ver Mais", NÃO vai abrir a página direto.
-// Vai abrir o popup.
+//pega os dois dialogs
+const sexoDialog = document.getElementById("sexoDialogo")
+const tipoDialog = document.getElementById("tipoDialog")
+
+//botoes de dialog sexo
+const btnSexoMasculino = document.getElementById("btnSexoMasculino")
+const btnSexoFeminino = document.getElementById("btnSexoFeminino")
+const btnCancelarSexo = document.getElementById("btnCancelarSexo")
+
+//botoes de dialog tipo
+const btnTipoOfical = document.getElementById("btnTipoOficial")
+const btnTipoSoldado = document.getElementById("btnTipoSoldado")
+const btnCancelarTipo = document.getElementById("btnCancelarTipo")
+
+//variavel que guarda escolha do sexo
+let sexoSelecionado = null; // "M" | "F"
+
+//evento de clicar no Ver mais e abre o pop de escolha sexo
 verMais.addEventListener("click", (e) => {
-    e.preventDefault(); // impede ir para taf.html automaticamente
-    sexoDialog.showModal(); // abre o popup
-});
+    e.preventDefault()
+    sexoDialog.showModal()
+})
 
-// 2) Se clicar em Homem, redireciona para uma página
-btnMasculino.addEventListener("click", () => {
-    window.location.href = "taf-m.html";
-});
-
-// 3) Se clicar em Mulher, redireciona para outra página
-btnFeminino.addEventListener("click", () => {
-    window.location.href = "taf-f.html";
-});
-
-// 4) Cancelar fecha o popup
-btnCancelar.addEventListener("click", () => {
-    sexoDialog.close();
-});
-/* fim do script de escolha*/
+//apos o escolha sexo ele abre o pop up do tipo
+btnSexoMasculino.addEventListener("click", () => {
+    sexoSelecionado = "M"
+    sexoDialog.close()
+    tipoDialog.showModal()
+})
